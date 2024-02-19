fcd1271894d209f3aa679e11a8f65422f58e7b5a
Alejandro López Labajo <mainusone1@gmail.com>
2024-02-19 12:41:23 +0100

    Commit

--- MvcNetCoreEF/Controllers/HospitalesController.cs
+++ MvcNetCoreEF/Controllers/HospitalesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using MvcNetCoreEF.Models;
 using MvcNetCoreEF.Repositories;
 
@@ -17,6 +18,44 @@ namespace MvcNetCoreEF.Controllers
         {
             List<Hospital> hospitales = this.repo.GetHospitales();
             return View(hospitales);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Hospital hospital = this.repo.FindHospital(id);
+            return View(hospital);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Hospital hospital)
+        {
+            this.repo.InsertHospital(hospital);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            this.repo.DeleteHospital(id);
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Update(int id)
+        {
+            Hospital hospital = this.repo.FindHospital(id);
+            return View(hospital);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Hospital hospital)
+        {
+            this.repo.UpdateHospital(hospital.HospitalCod, hospital.Nombre, hospital.Direccion, hospital.Telefono, hospital.NumCamas);
+            return RedirectToAction("Index");
         }
     }
 }
--- MvcNetCoreEF/Repositories/RepositoryHospital.cs
+++ MvcNetCoreEF/Repositories/RepositoryHospital.cs
@@ -1,4 +1,5 @@
-﻿using MvcNetCoreEF.Data;
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcNetCoreEF.Data;
 using MvcNetCoreEF.Models;
 
 namespace MvcNetCoreEF.Repositories
@@ -20,5 +21,41 @@ namespace MvcNetCoreEF.Repositories
                            select datos;
             return consulta.ToList();
         }
+
+        public Hospital FindHospital(int idHospital)
+        {
+            var consulta = from datos in this.context.Hospitales
+                           where datos.HospitalCod == idHospital
+                           select datos;
+
+            // Tenemos un método que devuelve el primer registro
+            // o el valor por defecto del model
+            return consulta.FirstOrDefault();
+        }
+
+        public void InsertHospital(Hospital hospital)
+        {
+            this.context.Hospitales.Add(hospital);
+            this.context.SaveChanges();
+        }
+
+        public void DeleteHospital(int idHospital)
+        {
+            Hospital hospital = this.FindHospital(idHospital);
+            this.context.Hospitales.Remove(hospital);
+            this.context.SaveChanges();
+        }
+
+        public void UpdateHospital(int id, string nombre, string direccion, string telefono, int camas)
+        {
+            Hospital hospital = this.FindHospital(id);
+            hospital.HospitalCod = id;
+            hospital.Nombre = nombre;
+            hospital.Direccion = direccion;
+            hospital.Telefono = telefono;
+            hospital.NumCamas = camas;
+            this.context.SaveChanges();
+        }
+
     }
 }
